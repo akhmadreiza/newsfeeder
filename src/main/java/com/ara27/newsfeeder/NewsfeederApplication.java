9b0d61d1c0afbf08c508ceed8108bf5affc73c89
Akhmad Reiza <reizaarmando@gmail.com>
2019-10-12 13:34:25 +0700

    init newsfeeder project and dependencies

---
+++ src/main/java/com/ara27/newsfeeder/NewsfeederApplication.java
@@ -0,0 +1,13 @@
+package com.ara27.newsfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsfeederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ara27/newsfeeder/NewsfeederApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ara27.newsfeeder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsfeederApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
