27c1f7f4af51639421ab228866201809606acc51
Akhmad Reiza <reizaarmando@gmail.com>
2019-10-30 02:09:05 +0700

    add debug mode to prevent sending email to real subscriber when debugging

--- src/main/java/com/ara27/newsfeeder/controller/NewsFeederController.java
+++ src/main/java/com/ara27/newsfeeder/controller/NewsFeederController.java
@@ -27,6 +27,7 @@ import java.util.List;
 public class NewsFeederController {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(NewsFeederController.class);
+    public static final String EMAIL_DEBUG_MODE = "reizaarmando@gmail.com";
 
     @Autowired
     DetikService detikService;
@@ -41,7 +42,8 @@ public class NewsFeederController {
     UserService userService;
 
     @GetMapping("/all")
-    public ResponseEntity getAll(@RequestParam(required = false) boolean sendEmail) throws IOException {
+    public ResponseEntity getAll(@RequestParam(required = false) boolean sendEmail,
+                                 @RequestParam(required = false, defaultValue = "true") boolean debugMode) throws IOException {
         List<Articles> finalList = new ArrayList<>();
         List<Articles> tirto = tirtoArticles();
         List<Articles> detik = detikArticles();
@@ -50,7 +52,8 @@ public class NewsFeederController {
         finalList.addAll(detik);
 
         if (sendEmail) {
-            sendEmail(tirto, detik);
+            List<String> emailRecipient = debugMode ? constructEmailRecipients(EMAIL_DEBUG_MODE) : constructEmailRecipients();
+            sendEmail(tirto, detik, emailRecipient);
         }
 
         return new ResponseEntity<>(new Data(finalList, finalList.size()), HttpStatus.OK);
@@ -61,13 +64,11 @@ public class NewsFeederController {
         LOGGER.info("feedme job invoked!");
         List<Articles> tirto = tirtoArticles();
         List<Articles> detik = detikArticles();
-        sendEmail(tirto, detik);
+        List<String> emailRecipient = constructEmailRecipients();
+        sendEmail(tirto, detik, emailRecipient);
     }
 
-    private void sendEmail(List<Articles> tirto, List<Articles> detik) throws IOException {
-        List<String> recipients = getAllRecipients();
-        /*recipients.add("reizaarmando@gmail.com");*/
-
+    private void sendEmail(List<Articles> tirto, List<Articles> detik, List<String> recipients) throws IOException {
         LOGGER.info("Start sending email to list of recipients");
         for (String recipient : recipients) {
             LOGGER.info(recipient);
@@ -77,7 +78,15 @@ public class NewsFeederController {
         LOGGER.info("Email sent to recipients!");
     }
 
-    private List<String> getAllRecipients() {
+    private List<String> constructEmailRecipients(String... recipient) {
+        List<String> recipients = new ArrayList<>();
+        for (String s : recipient) {
+            recipients.add(s);
+        }
+        return recipients;
+    }
+
+    private List<String> constructEmailRecipients() {
         List<UserEntity> allUsers = userService.getAllSubscriber();
         List<String> emails = new ArrayList<>();
         allUsers.forEach(userEntity -> {
@@ -92,68 +101,5 @@ public class NewsFeederController {
 
     private List<Articles> detikArticles() throws IOException {
         return detikService.listOfDetikPopularNews();
-    }
-
-    private String emailText(List... contents) throws IOException {
-        String text = "Hi there!" +
-                "\n \n" +
-                "Saya Akhmad Reiza Armando dan kalian pasti kenal saya wkwk. " +
-                "Kalian menerima email ini karena kalian adalah keluarga saya, pacar saya, teman saya atau rekan kerja saya :D\n\n" +
-
-                "Sebelumnya, saya mau ngucapin terima kasih buat kalian yang udah mau (dipaksa wkwk) menerima newsletter ini. " +
-                "Kalo kalian males menerima email ini lagi, silakan hubungi saya via channel apapun (WA boleh). " +
-                "Oiya sama satu lagi. Kalo kalian rasa keluarga, pacar, teman, bos, kalian membutuhkan email ini, silakan kirimkan alamat email nya ke email" +
-                "pribadi saya di reizaarmando@gmail.com dengan subject [FeedMe! Subscription Request]. Oke?" +
-
-                "\n\nMohon maaf dan thank you yaaa gaes sebelumnya :D\n" +
-                "\n" +
-
-                "Anyway, kalo boleh cerita dikit, jadi email ini tuh hasil generate-an dari versi early development dari program yang saya buat. " +
-                "Program yang saya buat ini namanya FeedMe! Apa itu? FeedMe! adalah sebuah program web-crawler yang nge rangkum berita populer dari beberapa sumber. " +
-                "Saat ini sayangnya sumber berita yang saya ambil baru dari dua sumber aja, yaitu detik.com dan tirto.id. " +
-                "Kedepannya akan saya tambahin lagi source nya yaa gengs!\n\n" +
-                "Beklah, langsung saja!" +
-                "\n\n" +
-                "Berikut adalah artikel dan berita populer saat ini: ";
-
-        text = text + selectedArticlesAndNews(contents[0], contents[1]);
-
-        //footer
-        text = text + "\n\n\n" + "Thanks and Regards,\nAkhmad Reiza Armando from FeedMe!";
-
-        return text;
-    }
-
-    private String selectedArticlesAndNews(List<Articles> tirto, List<Articles> detik) {
-        int tirtoIndex = 1;
-        int detikIndex = 1;
-
-        String text = "\n\n" +
-                "";
-
-        text = text + "Populer di Tirto - https://tirto.id\n\n";
-        for (Articles articles : tirto) {
-            if (articles.getSource().equalsIgnoreCase("tirto.id-populer") ||
-                    articles.getSource().equalsIgnoreCase("tirto.id-indept")) {
-                text = text + tirtoIndex + ". " + articles.getTitle() + " | " + articles.getUrl() + "\n\n";
-                tirtoIndex++;
-            }
-        }
-
-        text = text + "\n";
-
-        text = text + "Populer di Detik - https://detik.com\n\n";
-        for (Articles articles : detik) {
-            if (articles.getSource().equalsIgnoreCase("detikNews")
-                    || articles.getSource().equalsIgnoreCase("detikHot")
-                    || articles.getSource().equalsIgnoreCase("detikFinance")) {
-                text = text + detikIndex + ". " + articles.getTitle() + " | " + articles.getUrl() + "\n\n";
-                detikIndex++;
-            }
-        }
-
-        text = text + "\n";
-
-        return text;
     }
 }
